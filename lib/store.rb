8d32f31c72247cc82bc7612868ecbd76eee37b11
araff-16 <alex_raffa16@hotmail.com>
2022-01-04 00:40:01 +0000

    validations for the store table attributes

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to:0 }
+  validate :must_carry_at_least_mens_or_womens_Apparel
+
+  def must_carry_at_least_mens_or_womens_Apparel
+    if !(mens_apparel || womens_apparel)
+      errors.add(:mens_apparel, "Must carry at least mens or womens apparel")
+    end
+  end
+
 end
